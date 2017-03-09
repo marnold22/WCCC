200aaf6b3b65241890569140038f7796d157e0af
Tyler Reardon <tdr.reardon@gmail.com>
2017-03-08 21:24:20 -0800

    Scripts work for wordpress

--- index.php
+++ index.php
@@ -3,10 +3,9 @@
   <head>
     <title>WCCC</title>
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js"></script>
-    <script type="text/javascript" src="./resources/js/require.js"></script>
-    <script type="text/javascript">requirejs(["<?php echo get_template_directory_uri().'./resources/js/parallax.js'?>"], function(){ })</script>
-    <script type="text/javascript">requirejs(["<?php echo get_template_directory_uri().'./resources/js/main.js'?>"], function(){ })</script><link href=<?php echo get_template_directory_uri().'./resources/css/styles.css'?> rel="stylesheet">
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js"></script><script src=<?php echo get_template_directory_uri().'/assets/js/require.js' ?> ></script>
+    <script type="text/javascript">requirejs(["<?php echo get_template_directory_uri().'/assets/js/parallax.js'?>"], function(){ })</script>
+    <script type="text/javascript">requirejs(["<?php echo get_template_directory_uri().'/assets/js/main.js'?>"], function(){ })</script><link href=<?php echo get_template_directory_uri().'/assets/css/styles.css'?> rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1">
   </head>
   <body>
