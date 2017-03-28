0581439e6438a174b83a59e0f5b7ff4c53e2f224
Tyler Reardon <tdr.reardon@gmail.com>
2017-03-27 22:12:29 -0700

    Generated code

--- header.php
+++ header.php
@@ -4,7 +4,9 @@
     <meta charset="utf-8" />
     <title>Home</title>
     <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri().'/assets/css/styles.css';?>">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.4/lodash.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="<?php echo get_template_directory_uri().'/assets/js/parallax.js'; ?>"</script>
     <?php wp_head(); ?>
   </head>
 
