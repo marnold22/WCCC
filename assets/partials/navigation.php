e53cd1cf7dbb6d6329634c27c1a3902f2deed221
Tyler Reardon <tdr.reardon@gmail.com>
2017-04-06 11:54:00 -0700

    Added logo

--- assets/partials/navigation.php
+++ assets/partials/navigation.php
@@ -1,15 +1,19 @@
 <script defer src="<?php echo get_template_directory_uri().'/assets/js/navigation.js'; ?>"></script>
 
 <div class="nav-bar">
-  <nav>
-    <ul>
-      <li><a href="./about.php">About</a></li>
-      <li><a href="./programs.php">Programs</a></li>
-      <li><a href="./events.php">Events</a></li>
-      <li><a href="./giving.php">Giving</a></li>
-      <li><a href="./resources.php">Resources</a></li>
-      <li><a href="./contact.php">Contact</a></li>
-    </ul>
-    <div class="nav-bar-highlighter"></div>
-  </nav>
+    <div class="nav-bar-content">
+        <div class="nav-bar-logo" style="background-image: url(<?php echo get_template_directory_uri().'/assets/images/grey_logo.png'; ?>)"><a href="#"></a></div>
+
+        <nav>
+            <ul>
+              <li><a href="./about.php">About</a></li>
+              <li><a href="./programs.php">Programs</a></li>
+              <li><a href="./events.php">Events</a></li>
+              <li><a href="./giving.php">Giving</a></li>
+              <li><a href="./resources.php">Resources</a></li>
+              <li><a href="./contact.php">Contact</a></li>
+            </ul>
+            <div class="nav-bar-highlighter"></div>
+        </nav>
+    </div>
 </div>
