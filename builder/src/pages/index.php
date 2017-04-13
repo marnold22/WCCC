2a96ed5c14ec77122a98053fed22642e78205fe1
Tyler Reardon <tdr.reardon@gmail.com>
2017-04-12 21:35:34 -0700

    Event components

--- builder/src/pages/index.php
+++ builder/src/pages/index.php
@@ -4,7 +4,13 @@
     get_template_part('assets/partials/image-slider');
 
     require_once(get_template_directory().'/assets/partials/event.php');
-    echo createEventComponent('hi', 'there', 'what is up?', true);
+
+    $left = true;
+    for ($i=1; $i <= 10; $i++) {
+        echo createEventComponent(array('title'=>'title', 'image_ref'=>'http://westcentralcc.org/wp-content/uploads/2013/10/gym-72391-e1381356627607.jpg', 'content'=>'this is the contentlkjl;faksjdf;lkasjd;flk jasd;lf kjas;l dkjf ; lak', 'image_on_left'=>$left));
+        // $left = !($left);
+    }
+
 
 
   // get_template_part('assets/partials/where-we-are');
--- builder/src/resources/components/event/event.php
+++ builder/src/resources/components/event/event.php
@@ -2,12 +2,15 @@
     require_once(get_template_directory().'/assets/util/Mustache/Autoloader.php');
     Mustache_Autoloader::register();
 
-    function createEventComponent($title, $imageRef, $content, $imageOnLeft){
+    //array('title'=>$title, 'image_ref'=>$image_ref, 'content'=>$content, 'class'=>$sideClass)
+    function createEventComponent($args){
         //setup
         $m = new Mustache_Engine(array(
             'loader' => new Mustache_Loader_FilesystemLoader(get_template_directory().'/assets/templates')
         ));
-        $args = array('title'=>$title, 'imageRef'=>$imageRef, 'content'=>$content);
+
+        $sideClass = ($args['image_on_left'] ? 'left' : 'right');
+        $args['class'] = $sideClass;
 
         $tpl = $m->loadTemplate('event-template');
         return $tpl->render($args);
