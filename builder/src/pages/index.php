0b5cdec16330472f984af0280b1abc03cc13deaf
Tyler Reardon <tdr.reardon@gmail.com>
2017-04-19 11:58:27 -0700

    Add call to action

--- builder/src/pages/index.php
+++ builder/src/pages/index.php
@@ -8,7 +8,7 @@
     require_once(get_template_directory().'/assets/partials/full-width-image.php');
     require_once(get_template_directory().'/assets/partials/call-to-action.php');
 
-    echo createCallToActionComponent(array('title'=>'Donate', 'content'=>'this is the content of the message', 'button_link'=>'#this_is_the_link', 'button_title'=> 'Donate'));
+    echo createCallToActionComponent(array('title'=>'Donate', 'content'=>'Austin butcher tofu, thundercats green juice taxidermy cornhole four loko. Waistcoat butcher pinterest, try-hard four loko fashion axe disrupt fingerstache. Wolf sriracha direct trade leggings art party, godard disrupt gastropub. Health goth kinfolk williamsburg, pok pok crucifix photo booth wayfarers. Leggings iceland meggings 8-bit, cray sriracha forage shoreditch YOLO chicharrones raclette single-origin coffee. Cronut microdosing sartorial bushwick, stumptown quinoa brunch direct trade typewriter. Bicycle rights +1 flannel biodiesel leggings tumeric, chicharrones celiac asymmetrical.', 'button_link'=>'#this_is_the_link', 'button_title'=> 'Donate'));
 
 
     echo createFullWidthImageComponent(array('image_ref'=>'http://westcentralcc.org/wp-content/uploads/2013/10/gym-72391-e1381356627607.jpg'));
