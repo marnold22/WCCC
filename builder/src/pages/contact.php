8bbf9a5d6ab0c97b6c93503c2214f926682c037c
Tyler Reardon <tdr.reardon@gmail.com>
2017-04-19 21:09:58 -0700

    Add full width image to contact page

--- builder/src/pages/contact.php
+++ builder/src/pages/contact.php
@@ -1,6 +1,9 @@
 <?php /* Template Name: contact-template */
   get_header();
 
+  require_once(get_template_directory().'/assets/partials/full-width-image.php');
+  echo createFullWidthImageComponent(array('image_ref'=>'http://westcentralcc.org/wp-content/uploads/2013/10/gym-72391-e1381356627607.jpg'));
+
   echo do_shortcode( '[contact-form-7 id="42" title="Contact form 1"]' );
 
 
