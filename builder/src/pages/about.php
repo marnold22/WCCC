da0d39f1d1c44902930f97bcaf96f0e5054a1ec6
Tyler Reardon <tdr.reardon@gmail.com>
2017-04-23 16:43:45 -0700

    Can print text-image component from database

--- builder/src/pages/about.php
+++ builder/src/pages/about.php
@@ -1,11 +1,21 @@
 <?php /* Template Name: about-template */
+require_once(get_template_directory().'/assets/partials/text-image.php');
+
   get_header();
   $header_section = get_posts_for_category_and_tag('about', 'header-section');
 
   foreach ($header_section as $section) {
-      echo $section->post_title;
-      echo $section->post_content;
-      var_dump($header_section);
+      $images = get_attached_media( 'image', $section->ID );
+      $image_ref = '';
+      foreach ($images as $key => $image) {
+          $image_ref = $http.$image->guid;
+      }
+
+    //we get the first image if there is one in the post
+      $post_title = $section->post_title;
+      $post_content = $section->post_content;
+      $params = array('title'=>$post_title, 'image_ref'=>$image_ref, 'content'=>$post_content, 'image_on_left'=>true);
+      echo createTextImageComponent($params);
   }
 
   get_template_part('assets/partials/who-we-are');
