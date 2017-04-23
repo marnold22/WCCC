5931826e3ce4570b9f37e8f4e3bab77cb3d10c95
Tyler Reardon <tdr.reardon@gmail.com>
2017-04-23 16:03:37 -0700

    Working on page layout for about

--- builder/src/pages/about.php
+++ builder/src/pages/about.php
@@ -1,10 +1,14 @@
 <?php /* Template Name: about-template */
-
   get_header();
+  $header_section = get_posts_for_category_and_tag('about', 'header-section');
+
+  foreach ($header_section as $section) {
+      echo $section->post_title;
+      echo $section->post_content;
+      var_dump($header_section);
+  }
+
   get_template_part('assets/partials/who-we-are');
-
   get_template_part('assets/partials/timeline');
-
   get_footer();
-
 ?>
