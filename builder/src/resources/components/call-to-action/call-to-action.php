4f4c55217d4c8001c15c4eff5ec0f287b9aba523
Tyler Reardon <tdr.reardon@gmail.com>
2017-04-28 12:24:51 -0700

    Creating call to action compontents

--- builder/src/resources/components/call-to-action/call-to-action.php
+++ builder/src/resources/components/call-to-action/call-to-action.php
@@ -12,4 +12,34 @@
         $tpl = $m->loadTemplate('call-to-action');
         return $tpl->render($args);
     }
+
+    function createCallToActionComponentForCategoryAndTag($args){
+        //get the posts for the category and tag
+        $posts = get_posts_for_category_and_tags($args['category'], $args['tags']);
+        $params = array();
+        $components = array();
+        foreach ($posts as $post) {
+            $post_title = $post->post_title;
+            $post_content = $post->post_content;
+            //pattern for urls
+            $re = '/<a href="(.*)">(.*)<\/a>/';
+            //get the link from the post
+            $success = preg_match_all($re, $post_content, $matches, PREG_SET_ORDER, 0);
+            //if we successfully found a link
+            if ($success) {
+                $params['button_link'] = $matches[0][1];
+                $params['button_title'] = $matches[0][2];
+            }
+            //remove all the links from the post
+            $post_content = preg_replace($re, "", $post_content);
+            //strip the html and shortcodes
+            $post_content = wp_filter_nohtml_kses($post_content);
+            $post_content = strip_shortcodes($post_content);
+            $params['content'] = $post_content;
+            $params['title'] = $post_title;
+            $component = createCallToActionComponent($params);
+            array_push($components, $component);
+        }
+        return $components;
+    }
 ?>
