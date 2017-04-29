cde70cd28001ee87cdd3839ef92655d17759f210
Tyler Reardon <tdr.reardon@gmail.com>
2017-04-28 18:08:56 -0700

    Add half-half to events page

--- builder/src/pages/events.php
+++ builder/src/pages/events.php
@@ -1,10 +1,13 @@
 <?php /* Template Name: events-template */
   get_header();
-
-  $components = createTextImageContentSection(array('category'=>'Events', 'image_on_left'=>true));
+  $category = 'Events';
+  $components = createTextImageContentSection(array('category'=>$category, 'image_on_left'=>true));
   foreach ($components as $component) { echo $component; }
 
-  get_template_part('assets/partials/calendar');
+  // get_template_part('assets/partials/calendar');
+  $image = createFullWidthImageComponentForCategoryAndTags(array('category'=>$category, 'tags'=>array('full-width-image', 'header')));
+  $calendar = createCalendarComponent(array('shortcode'=>'[calendar id="108"]'));
+  echo createHalfHalfComponent(array('left_content'=>$image, 'right_content'=>$calendar));
 
   get_footer();
 ?>
