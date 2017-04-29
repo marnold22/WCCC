54e7b21f2b5695c492591e9b908fe15e34a48f06
Tyler Reardon <tdr.reardon@gmail.com>
2017-04-28 18:08:20 -0700

    Make calendar component dynamic

--- builder/src/resources/components/calendar/calendar.php
+++ builder/src/resources/components/calendar/calendar.php
@@ -1,3 +1,20 @@
-<div class="calendar-div">
-  <?php echo do_shortcode('[calendar id="108"]'); ?>
-</div>
+<?php
+
+require_once(get_template_directory().'/assets/util/Mustache/Autoloader.php');
+Mustache_Autoloader::register();
+
+//array('shortcode'=>$shortcode)
+function createCalendarComponent($args){
+    //setup
+    $m = new Mustache_Engine(array(
+        'loader' => new Mustache_Loader_FilesystemLoader(get_template_directory().'/assets/templates')
+    ));
+
+    if($args['shortcode']){
+        $args['shortcode'] = do_shortcode($args['shortcode']);
+    }
+
+    $tpl = $m->loadTemplate('calendar');
+    return $tpl->render($args);
+}
+?>
