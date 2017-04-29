e561399a4fd24270ab23bf46fa9d41e95f42bd1d
Tyler Reardon <tdr.reardon@gmail.com>
2017-04-29 10:49:57 -0700

    Add custom buttons to call-to-action component

--- builder/src/resources/components/call-to-action/call-to-action.php
+++ builder/src/resources/components/call-to-action/call-to-action.php
@@ -1,15 +1,26 @@
 <?php
-    //array('title'=>$title, 'content'=>$content, 'button_link'=>$button_link, 'button_title'=>$button_title)
+    //array('title', 'content', 'button', 'button_type', 'button_link', 'button_title')
     function createCallToActionComponent($args){
         //setup
         $m = new Mustache_Engine(array(
             'loader' => new Mustache_Loader_FilesystemLoader(get_template_directory().'/assets/templates')
         ));
 
+        //if we don't have a button
+        if(!$args['button']){
+            //create button component based on parameters
+            $isSubmitButton = ($args['button_type'] === 'submit');
+            $buttonParams = array('button_link'=>$args['button_link'], 'button_title'=>$args['button_title'], 'button_type'=>$args['button_type']);
+            $button = createLinkButtonComponent($buttonParams);
+            //add the button to this function's arguments
+            $args['button'] = $button;
+        }
+
         $tpl = $m->loadTemplate('call-to-action');
         return $tpl->render($args);
     }
 
+    //array('category', 'tag', 'button_type', 'button')
     function createCallToActionComponentForCategoryAndTag($args){
         //get the posts for the category and tag
         $posts = get_posts_for_category_and_tags($args['category'], $args['tags']);
@@ -18,15 +29,24 @@
         foreach ($posts as $post) {
             $post_title = $post->post_title;
             $post_content = $post->post_content;
-            //pattern for urls
-            $re = '/<a href="(.*)">(.*)<\/a>/';
-            //get the link from the post
-            $success = preg_match_all($re, $post_content, $matches, PREG_SET_ORDER, 0);
-            //if we successfully found a link
-            if ($success) {
-                $params['button_link'] = $matches[0][1];
-                $params['button_title'] = $matches[0][2];
+
+            $post_tags = get_tags_for_post($post->ID);
+            //if the post isn't meant to be a paypal donation
+            if(!in_array('paypal', $post_tags)){
+                //pattern for urls
+                $re = '/<a href="(.*)">(.*)<\/a>/';
+                //get the link from the post
+                $success = preg_match_all($re, $post_content, $matches, PREG_SET_ORDER, 0);
+                //if we successfully found a link
+                if ($success) {
+                    $params['button_link'] = $matches[0][1];
+                    $params['button_title'] = $matches[0][2];
+                }
+            }else{
+                //we want a paypal button
+                $params['button'] = createGivingPayPalComponent();
             }
+
             //remove all the links from the post
             $post_content = preg_replace($re, "", $post_content);
             //strip the html and shortcodes
