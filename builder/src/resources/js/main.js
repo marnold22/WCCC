7bb90e5d226ce45dd08c81e5c9dd2edd85e8b551
Tyler Reardon <tdr.reardon@gmail.com>
2017-05-06 12:22:50 -0700

    Main.js comments

--- builder/src/resources/js/main.js
+++ builder/src/resources/js/main.js
@@ -1,5 +1,7 @@
 //start the script
 $(document).ready(()=>{
+
+    //set up the navigation bar
     let navBars = $('.nav-bar');
     for(let i = 0; i < navBars.length; i++){
         let bar = new NavBar(navBars[i]);
