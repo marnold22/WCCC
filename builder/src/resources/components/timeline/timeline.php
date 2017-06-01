45995e5ea627ebf2facc535232324ebc282ae378
marnold22 <minimearnold@gmail.com>
2017-06-01 10:02:25 -0700

    Added content to timeline

--- builder/src/resources/components/timeline/timeline.php
+++ builder/src/resources/components/timeline/timeline.php
@@ -17,11 +17,13 @@ function createTimelineComponent($args){
 <section class="timeline" id="timeline" >
 	<div class="demo-card-wrapper">
 
-		<?php
-			for($i = 1; $i < 6; $i++){
-				echo createTimelineComponent(array('number'=>$i, 'subtitle'=>'subtitle', 'title'=>'title', 'description'=>'description description description description description description description description description description description description description description ', 'imageRef'=>'http://placehold.it/1000x500'));
-			}
-		?>
+		<?php echo createTimelineComponent(array('number'=>'1', 'subtitle'=>'', 'title'=>'1981-1991', 'description'=>'The West Central Community Center opens its doors to serve the neighborhoods of West Central, Emerson-Garfield and Northwest (now Audubon/Downriver) providing child care, health services, youth recreation and development and supportive services for developmentally disabled adults.', 'imageRef'=>'http://placehold.it/1000x500'));?>
+
+		<?php echo createTimelineComponent(array('number'=>'2', 'subtitle'=>'', 'title'=>'1991-2001', 'description'=>'Neighborhood improvement initiatives are instituted to address code violations and crime and to create a sense of community for the families we serve.  Project Pride, COPS-WEST and the West Central Neighborhood Coalition are among the programs established.', 'imageRef'=>'http://placehold.it/1000x500'));?>
+
+		<?php echo createTimelineComponent(array('number'=>'3', 'subtitle'=>'', 'title'=>'2001-2010', 'description'=>'The West Central Community Center is expanded to address growing neighborhood needs and includes additional classrooms for pre-kindergarten children and expanded medical facilities.  WCCC is now serving more than 4,000 individuals annually.', 'imageRef'=>'http://placehold.it/1000x500'));?>
+
+		<?php echo createTimelineComponent(array('number'=>'4', 'subtitle'=>'', 'title'=>'2010-Present', 'description'=>'Youth Development programming now includes Outdoor Wilderness Learning.  Funding is achieved to build a five-bay garage for Center vehicles.  Spokane Police Department has established a permanent presence on site.  Women, Infants and Children serves 3,000 clients monthly.  And, Supportive Services has grown to be the second largest program for disabled adults in the County.', 'imageRef'=>'http://placehold.it/1000x500'));?>
 
 	</div>
 </section>
