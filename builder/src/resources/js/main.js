5fe409ec6b343733451cc75901e765366cc5b3a7
Tyler Reardon <tdr.reardon@gmail.com>
2017-06-01 19:04:55 -0700

    Fix script loading issues

--- builder/src/resources/js/main.js
+++ builder/src/resources/js/main.js
@@ -1,9 +1,25 @@
-//start the script
-$(document).ready(()=>{
+function loadScript(script, callback){
+    //load jQuery
+    var myScript = document.createElement('script');
+    myScript.onload = function() {
+        if(typeof callback === 'function'){
+            callback();
+        }
+    };
+    myScript.src = script;
+    document.getElementsByTagName('head')[0].appendChild(myScript);
+}
 
-    //set up the navigation bar
-    let navBars = $('.nav-bar');
-    for(let i = 0; i < navBars.length; i++){
-        let bar = new NavBar(navBars[i]);
-    }
+window.addEventListener("load", function(){
+    //load jQuery
+    var jqueryScript = document.createElement('script');
+    jqueryScript.onload = function() {
+        //set up the navigation bar
+        let navBars = $('.nav-bar');
+        for(let i = 0; i < navBars.length; i++){
+            let bar = new NavBar(navBars[i]);
+        }
+    };
+    jqueryScript.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js';
+    document.getElementsByTagName('head')[0].appendChild(jqueryScript);
 });
