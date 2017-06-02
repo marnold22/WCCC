c1c8db44287ac4d4eaaa70cd8f920811c1bdfc2a
Tyler Reardon <tdr.reardon@gmail.com>
2017-06-02 11:27:22 -0700

    New build

--- WCCC2017/giving.php
+++ WCCC2017/giving.php
@@ -15,12 +15,10 @@
   $components = createTextImageComponentsForCategoryAndTags(array('category'=>$category, 'tags'=>array('text-image'), 'number_desired'=>null, 'image_on_left'=>false));
   print_component_array($components);
 
-
   //get_template_part('assets/partials/partners-spotlight');
-  echo createHeadingComponent(array('title'=>'Donor Spotlight'));
+
+  echo createHeadingComponent(array('title'=>'Donor Spotlight', 'classes'=>'blue-background'));
   echo do_shortcode('[abcf-staff-list id="83"]');
-
-
 
   // $partners = array('City of Spokane', 'County of Spokane', 'Emmerson-Garfield Neighborhood Council', 'Empire Health Foundation', 'Spokane Parks and Recreation', 'Washington State Department of Health', 'Welty Foundation', 'West Central Neighborhood Council', 'Women Helping Women', 'Airway Heights Community Center', 'Cheney United Methodist Church', 'Community Prevention and Wellness Initiative of Spokane County', 'Fairchild Airforce Base', 'Greater Spokane Substance Abuse Council', 'I-Choice', 'Moling Healthcare', 'Our Place', 'Project Hope', 'Second Harvest Food Bank', 'Spokane COPS', 'Spokane Falls Family Clinic', 'Spokane Hoopfest', 'Spokane Public Schools', 'Spokane Regional Health District', 'West Spokane Kiwanis', 'Youth for Christ');
   echo createHeadingComponent(array('title'=>'Our Partners'));
